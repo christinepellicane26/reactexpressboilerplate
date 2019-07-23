bb4a1ee02f1591e3e9e152022aeaa776905b0c06
Student <jsitbon@qcc.cuny.edu>
2019-07-23 17:44:11 -0400

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const port = 4000;
+const port = 4000
+
 
 //Middleware
 app.use(morgan('dev'));
@@ -12,11 +14,60 @@ app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
 });
 
-app.get('/api/students', (req, res) => {
-  const students = [
-    { id: 1, firstName: 'Captain', lastName: 'fancy' },
-    { id: 2, firstName: 'John', lastName: 'buttercup' },
-    { id: 3, firstName: 'Dusty', lastName: 'Trail' },
-  ];
-  res.json(students);
+app.get('/employees', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Employee', 
+    (err,rows,field) => {
+    if (!err) res.send(rows);
+    else console.log(err);
+
+  });
+
+app.get('/employees/:id', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Employee WHERE EmpID =?', 
+  [req.params.id], 
+  (err,rows,field) => {
+    if (!err) res.send(rows);
+    else console.log(err);
+  
+  });
+
+  app.delete('/employees/:id', (req, res) => {
+    mysqlConnection.query(
+      'Delete FROM Employee WHERE EmpID =?', 
+    [req.params.id], 
+    (err,rows,field) => {
+      if (!err) res.send('Deleted Successfully');
+      else console.log(err);
+   
 });
+
+// app.post('/employees', (req, res)=> {
+//   let emp =req.body;
+//   let sql ='SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary =?; \
+//     CALL EmployeeAndOrEdit(@EmpID, @Name, @EmpCode, @Salary);';
+//     mysqlConnection.query(
+//       sql,[emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+//       [req.params.id],
+//       (err, rows, field) => {
+//         if(!err) res.send(rows);
+//         else console.log(err);
+//       }
+//     );
+// })
+
+const mysqlConnection = mysql.createConnection({
+host: '10.9.3.218',
+user: 'TWStudent',
+password: 'TechWorks!',
+database: 'employeedb',
+multipleStatements: true
+});
+
+mysqlConnection.connect(err =>{
+  if (!err) console.log('DB connection succeeded');
+  else {
+    console.log(
+      `DB connection failed Error: ` + JSON.stringify(err, undefined, 2)
+    );
+  }
+})
